215c969c44113eb5263d567b75d38fc12a9e252b
unknown <ibmjavanh20@iiht.tech>
2021-03-30 21:23:36 +0530

    update by Rupak

--- project-service/src/main/java/com/ibm/projectbts/ProjectController.java
+++ project-service/src/main/java/com/ibm/projectbts/ProjectController.java
@@ -11,6 +11,7 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,21 +32,21 @@ public class ProjectController {
 		validateModel(bindingResult);
 		return projectService.createProject(project);
 	}
-	
+
 	@GetMapping("/project")
 	List<Project> getProject() {
-	return projectService.getProject();
+		return projectService.getProject();
 	}
 
 	/**
-	* method to obtain single project details
-	*
-	* @param projectId
-	* @return project details
-	*/
+	 * method to obtain single project details
+	 *
+	 * @param projectId
+	 * @return project details
+	 */
 	@GetMapping("/project/{id}")
 	Optional<Project> getProject(@PathVariable("id") String projectId) {
-	return projectService.getProject(projectId);
+		return projectService.getProject(projectId);
 	}
 
 	/**
@@ -57,6 +58,21 @@ public class ProjectController {
 		if (bindingResult.hasErrors()) {
 			throw new IllegalArgumentException("Something went wrong. Plesae retry");
 		}
+	}
+
+	/**
+	 * updates project details
+	 *
+	 * @param projectId
+	 * @param project
+	 * @param bindingResult
+	 */
+	@PutMapping("/project/{id}")
+	void updateProject(@PathVariable("id") String projectId, @RequestBody Project project,
+			BindingResult bindingResult) {
+		validateModel(bindingResult);
+		project.setId(projectId);
+		projectService.updateProject(project);
 	}
 
 }
--- project-service/src/main/java/com/ibm/projectbts/ProjectService.java
+++ project-service/src/main/java/com/ibm/projectbts/ProjectService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class ProjectService {
@@ -15,12 +16,17 @@ public class ProjectService {
 		Project savedProject = projectRepository.save(project);
 		return savedProject.getId();
 	}
+
+	public void updateProject(@RequestBody Project project) {
+		projectRepository.save(project);
+	}
+
 	public List<Project> getProject() {
 		return projectRepository.findAll();
-		}
+	}
 
-		public Optional<Project> getProject(String projectId) {
+	public Optional<Project> getProject(String projectId) {
 		return projectRepository.findById(projectId);
-		}
+	}
 
 }

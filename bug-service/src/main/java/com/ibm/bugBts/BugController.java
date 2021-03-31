c04fae36b1f2546c80e9d8361e18ab8ee0ff9ec4
unknown <ibmjavanh08@iiht.tech>
2021-03-31 15:15:27 +0530

    GET METHOD

--- bug-service/src/main/java/com/ibm/bugBts/BugController.java
+++ bug-service/src/main/java/com/ibm/bugBts/BugController.java
@@ -1,10 +1,15 @@
 package com.ibm.bugBts;
 
+import java.util.List;
+import java.util.Optional;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +29,16 @@ public class BugController {
 		if (bindingResult.hasErrors()) {
 			throw new IllegalArgumentException("Something went wrong. Plesae retry");
 		}
+	}
+	
+	@GetMapping("/bug")
+	List<Bug> getBugs() {
+		return bugService.getBugs();
+	}
+	
+	@GetMapping("/project/{id}")
+	Optional<Bug> getBug(@PathVariable("id") String bugId) {
+		return bugService.getBug(bugId);
 	}
 
 }
--- bug-service/src/main/java/com/ibm/bugBts/BugService.java
+++ bug-service/src/main/java/com/ibm/bugBts/BugService.java
@@ -1,7 +1,12 @@
 package com.ibm.bugBts;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+
 
 @Service
 public class BugService {
@@ -12,5 +17,12 @@ public class BugService {
 		Bug savedBug = bugRepository.save(bug);
 		return savedBug.getId();
 	}
+	public List<Bug> getBugs() {
+		return bugRepository.findAll();
+	}
+	public Optional<Bug> getBug(String bugId) {
+		return bugRepository.findById(bugId);
+	}
+
 
 }

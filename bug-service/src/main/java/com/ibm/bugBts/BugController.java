2de5ba5f19d96ee1e28066d4a472049a5e38c713
unknown <ibmjavanh08@iiht.tech>
2021-04-09 17:19:26 +0530

    create bug method

--- bug-service/src/main/java/com/ibm/bugBts/BugController.java
+++ bug-service/src/main/java/com/ibm/bugBts/BugController.java
@@ -21,32 +21,25 @@ public class BugController {
 	BugService bugService;
 
 	@PostMapping("/bug")
-	String createBug(@RequestBody @Valid Bug bug, BindingResult bindingResult) {
-		validateModel(bindingResult);
-		return bugService.createBug(bug);
+	int createBug(@RequestBody Bug bug) {
+		return bugService.save(bug);
 	}
 
-	private void validateModel(Errors bindingResult) {
-		if (bindingResult.hasErrors()) {
-			throw new IllegalArgumentException("Something went wrong. Plesae retry");
-		}
-	}
 
-	@GetMapping("/bug")
-	List<Bug> getBugs() {
-		return bugService.getBugs();
-	}
-
-	@GetMapping("/bug/{id}")
-	Optional<Bug> getBug(@PathVariable("id") String bugId) {
-		return bugService.getBug(bugId);
-	}
-
-	@PutMapping("/bug/{id}")
-	void updateProject(@PathVariable("id") String bugId, @RequestBody Bug bug, BindingResult bindingResult) {
-		validateModel(bindingResult);
-		bug.setId(bugId);
-		bugService.updateBug(bug);
-	}
-
+//	@GetMapping("/bug")
+//	List<Bug> getBugs() {
+//		return bugService.getBugs();
+//	}
+//
+//	@GetMapping("/bug/{id}")
+//	Optional<Bug> getBug(@PathVariable("id") String bugId) {
+//		return bugService.getBug(bugId);
+//	}
+//
+//	@PutMapping("/bug/{id}")
+//	void updateProject(@PathVariable("id") String bugId, @RequestBody Bug bug) {
+//		bug.setId(bugId);
+//		bugService.updateBug(bug);
+//	}
+//
 }
---
+++ bug-service/src/main/java/com/ibm/bugBts/IBugService.java
@@ -0,0 +1,12 @@
+package com.ibm.bugBts;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IBugService {
+	int save(Bug bug);
+//	List<Bug> getBugs();
+//	Optional<Bug> getBug(int bugId);
+//	void updateBug(Bug bug);
+
+}

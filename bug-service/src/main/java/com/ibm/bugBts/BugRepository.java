5b7f75869d12a299c59ac60728d95eea68d23ccf
unknown <ibmjavanh08@iiht.tech>
2021-04-24 21:12:24 +0530

    Added Partial Search

--- bug-service/src/main/java/com/ibm/bugBts/BugRepository.java
+++ bug-service/src/main/java/com/ibm/bugBts/BugRepository.java
@@ -11,9 +11,11 @@ public interface BugRepository extends MongoRepository<Bug,String>{
 	Optional<Bug> findByName(String bugName);
 
 	List<Bug> findByStatus(String bugStatus);
-
+	
+	@Query("{status: ?0,name : {$regex : ?1,'$options':'i'}}")
 	List<Bug> getByStatusAndName(STATUS bugStatus, String bugName);
 
+	@Query("{name : {$regex : ?0,'$options':'i'}}")
 	List<Bug> findByNameIgnoreCase(String bugName);
 
 }

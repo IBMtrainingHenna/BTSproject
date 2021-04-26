593247147676a1e02001749ad521335347eeb8ea
unknown <ibmjavanh11@iiht.tech>
2021-04-27 01:23:56 +0530

    added getby status and na,e and case ignore

--- bug-service/src/main/java/com/ibm/bugBts/BugRepository.java
+++ bug-service/src/main/java/com/ibm/bugBts/BugRepository.java
@@ -7,9 +7,15 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
 public interface BugRepository extends MongoRepository<Bug,String>{
-//	@Query("{'name':?0}")
+
 	Optional<Bug> findByName(String bugName);
 
 	List<Bug> findByStatus(String bugStatus);
+
+	@Query("{name:{$regex:?0,'$options':'i'}}")
+	List<Bug> getByStatusAndNameIgnoreCase(String bugName, String bugStatus);
+
+	@Query("{name:{$regex:?0,'$options':'i'}}")
+	List<Bug> findNameIgnoreCase(String bugName);
 
 }

b8870e3e481eac397ecf781d9e2a3170b1edbb7a
unknown <ibmjavanh11@iiht.tech>
2021-04-27 01:22:19 +0530

    added partial name status and name and delete methods

--- bug-service/src/main/java/com/ibm/bugBts/BugController.java
+++ bug-service/src/main/java/com/ibm/bugBts/BugController.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import javax.validation.Valid;
+import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,15 +38,25 @@ public class BugController {
 	List<Bug> getBugs() {
 		return bugService.getBugs();
 	}
-
+	
 	@GetMapping("/bug/name/{name}")
 	Optional<Bug> getBugbyName(@PathVariable("name") String bugName) {
 		return bugService.getBugbyName(bugName);
+	}
+
+	@GetMapping("/bug/{name}")
+	List<Bug> getBugbyPartialName(@PathVariable("name") String bugName) {
+		return bugService.getBugbyPartialName(bugName);
 	}
 	
 	@GetMapping("/bug/status/{status}")
 	List<Bug> getBugbyStatus(@PathVariable("status") String bugStatus) {
 		return bugService.getBugbyStatus(bugStatus);
+	}
+	
+	@GetMapping("/bug/search/{name}/{status}")
+	List<Bug> getByStatusAndName(@PathVariable("name") String bugName, @PathVariable("status") String bugStatus) {
+		return bugService.getByStatusAndName(bugName, bugStatus);
 	}
 
 	@PutMapping("/bug/{id}")
@@ -52,5 +64,10 @@ public class BugController {
 		validateModel(bindingResult);
 		bug.setId(bugId);
 		bugService.updateBug(bug);
+	}
+	
+	@DeleteMapping("/bug/{id}")
+	void deleteBug(@PathVariable("id")String bugId) {
+		bugService.deleteBug(bugId);
 	}
 }
